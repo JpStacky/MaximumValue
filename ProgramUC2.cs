b437fc47d2213ecb0b689e80c0eac99833d7ee5b
Jp <jyotiprakashpc@outlook.com>
2022-08-10 08:25:30 +0530

    To find maximum float value

---
+++ ProgramUC2.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericsMaximumProblem
+{
+    class ProgramUC2
+    {
+        public static void Main()
+        {
+            MaximumFloatUC2 max = new MaximumFloatUC2();
+            max.GetInput();
+            max.Compare();
+        }
+    }
+}
