f591f3e4b0f8b827980efe2f8b485f57fdf4f063
Jp <jyotiprakashpc@outlook.com>
2022-08-10 08:21:58 +0530

    Welcome Code

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericsMaximumProblem
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome To Maximum Value Computation Program");
+        }
+    }
+}
